57d015a281054847a74d14d88ba813126100dc41
Lorenzo <arduinocarelli@gmail.com>
2023-02-26 13:47:58 +0100

    Finalize the code for the LCM and add the code used to test the GCD function
    This is the first version of the LCM calculator that calculates the LCM values from the a and b variables and uses a formula with the GCD to calculate the LCM.
    There is a separate file that was used to test out the GCD function that is called  "GCD calculator"

---
+++ C/LCM calculation/GCD calculator.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// this code was created to test out the GCD function for the LCM calculation code
+
+int a = 0;
+int b = 0;
+int c = 0;
+
+// define the GCD function
+int GCD(int a, int b);
+
+int main() {
+
+// input the numbers that we will be using to test out the GCD function
+	printf("Input the values for the first number");
+	scanf("%d", &a);
+	printf("Input the values for the second number");
+	scanf("%d", &b);
+	// print the value returned by the GCD function
+	printf("the GCD is %d", GCD(a, b));
+
+}
+
+GCD (int a, int b) {
+// Returns the greatest common divisor (GCD) of two integers
+
+int remainder;
+int gcd;
+
+if (a > b)
+	{
+		remainder = a % b;
+		// If remainder is not zero, keep looping until it is
+		if (remainder != 0)
+		{
+			while (remainder != 0)
+			{
+			gcd = remainder;
+			remainder = a % remainder;
+			}
+		} else gcd = b;
+		
+	} else {
+		remainder = b % a;
+		// If remainder is not zero, keep looping until it is
+		if (remainder != 0)
+		{
+			while (remainder != 0)
+			{
+			gcd = remainder;
+			remainder = b % remainder;
+			}
+		} else gcd = a;
+	}
+	return gcd;	// Return the GCD
+}
--- C/LCM calculation/LCM calculation.c
+++ C/LCM calculation/LCM calculation.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+	This code calculates the LCM (Least Common Multiple) between two numbers and returns it in a printable format
+	It works by calculating the GCD between the two numbers, and then using the formula LCM = (a*b)/GCD(a,b) to
+	Calculate the Least Common Multiple of those two numbers.
+*/
+
+int a = 0;
+int b = 0;
+int lcm;
+
+int GCD(int a, int b);	// define the GCD function
+
 int main()
 {
+	// input the numbers that will be used to calculate the LCM
+	printf("Input the values for the first number \n");
+	scanf("%d", &a);
+	printf("Input the values for the second number \n");
+	scanf("%d", &b);
+
+	// calculate the LCM by using the formula below
+	lcm = (a*b)/GCD(a, b);
 	
+	// print the LCM value
+	printf("The Least common multiple of %d and %d is %d", a, b, lcm);
 	return 0;
+}
+
+GCD (int a, int b) {
+// Returns the greatest common divisor (GCD) of two integers
+
+int remainder;
+int gcd;
+
+if (a > b)
+	{
+		remainder = a % b;
+		// If remainder is not zero, keep looping until it is
+		if (remainder != 0)
+		{
+			while (remainder != 0)
+			{
+			gcd = remainder;
+			remainder = a % remainder;
+			}
+		} else gcd = b;
+		
+	} else {
+		remainder = b % a;
+		// If remainder is not zero, keep looping until it is
+		if (remainder != 0)
+		{
+			while (remainder != 0)
+			{
+			gcd = remainder;
+			remainder = b % remainder;
+			}
+		} else gcd = a;
+	}
+	return gcd;	// Return the GCD
 }
