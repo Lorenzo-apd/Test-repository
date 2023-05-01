1f85efbcbaeca92d7d2ce93087cb51912d13c174
Lorenzo <arduinocarelli@gmail.com>
2023-05-01 14:58:20 +0200

    Complete the digit frequency challenge

--- C/Digit frequency/Digit frequency.c
+++ C/Digit frequency/Digit frequency.c
@@ -1,11 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-string s;
-char before, after;
+#define size 150
+
+void printArray(int *pointer);
+void findoccurrences(int *pointer, char *input);
 
 int main()
 {
-	/* code */
+
+	char num[size] = "1v88886l256338ar0ekk";
+	int repetitions [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+	findoccurrences(&repetitions[0], &num[0]);
+	printArray(&repetitions[0]);
 	return 0;
+
+}
+
+void printArray(int *pointer)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d, ", *pointer);
+		pointer++;
+	}
+}
+
+void findoccurrences(int *occurrence, char *input) {
+    while (*input != '\0') {
+        if (*input >= '0' && *input <= '9') {
+            (*(occurrence + (*input - '0')))++;
+        }
+        input++;
+    }
 }
